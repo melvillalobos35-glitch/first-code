f371730aa01d3b4812863b3a405d787ea14aa15d
bao yum <MelVillalobos35@gmail.com>
2026-06-25 18:17:55 +0800

    嵌套循环

--- 6yue25.py
+++ 6yue25.py
@@ -1,13 +1,4 @@
-class guest:
-    def __init__(self, name):
-        self.name = name
-    def invite(self):
-        if self.name == "Bob":
-            print(f"Dear {self.name}, you are not invited to the party.")
-        else:
-            print(f"Dear {self.name}, you are invited to the party.")
-guest1 = guest("Alice")
-guest2 = guest("Bob")
-guest3 = guest("Charlie")
-for guest in [guest1, guest2, guest3]:
-    guest.invite()
+for i in range(1, 4):
+    for j in range(i):
+        print("*", end="")
+    print()
