f7b6f82e5541331542c569060142ce91117d1c30
bao yum <MelVillalobos35@gmail.com>
2026-06-25 15:53:39 +0800

    我的第二个代码

--- 6yue25.py
+++ 6yue25.py
@@ -1,28 +1,7 @@
-gold = 100  # 全局变量
+# 读文件
+with open("6yue23.py", "r", encoding="utf-8") as f:
+    content = f.read()
 
-def show_gold():
-    # 只读取全局变量
-    print("当前金币：", gold)
-
-def buy_sword():
-    # 修改全局变量
-    global gold
-    gold -= 30
-    print("购买宝剑后金币：", gold)
-
-def test_local():
-    # 局部变量
-    gold = 999
-    print("局部金币：", gold)
-
-print("程序开始")
-
-show_gold()      # 读取全局变量
-
-test_local()     # 创建局部变量
-
-print("全局金币仍然是：", gold)
-
-buy_sword()      # 修改全局变量
-
-print("最终金币：", gold)
+# 写文件
+with open("6yue23.py", "w", encoding="utf-8") as f:
+    f.write("niahoynaaoap")
