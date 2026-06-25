f48927a6ceb15542b77f452101a611b042d26dd7
bao yum <MelVillalobos35@gmail.com>
2026-06-25 17:22:38 +0800

    遍历

--- 6yue25.py
+++ 6yue25.py
@@ -1,41 +1,13 @@
-class Person:
-
-    def __init__(self, name, age):
+class guest:
+    def __init__(self, name):
         self.name = name
-        self.age = age
-
-    def introduce(self):
-        print("姓名:", self.name)
-        print("年龄:", self.age)
-
-
-class Student(Person):
-
-    def __init__(self, name, age, score):
-        super().__init__(name, age)
-        self.score = score
-
-    def get_score(self):
-        return self.score
-
-    def set_score(self, score):
-        self.score = score
-
-    def study(self):
-        print(self.name, "正在学习")
-
-
-s1 = Student("张三", 18, 95)
-s2 = Student("李四", 19, 88)
-
-s1.introduce()
-s1.study()
-
-print(s1.get_score())
-
-s1.set_score(100)
-
-print(s1.get_score())
-s2.introduce()
-s2.study()
-print(s2.get_score())
+    def invite(self):
+        if self.name == "Bob":
+            print(f"Dear {self.name}, you are not invited to the party.")
+        else:
+            print(f"Dear {self.name}, you are invited to the party.")
+guest1 = guest("Alice")
+guest2 = guest("Bob")
+guest3 = guest("Charlie")
+for guest in [guest1, guest2, guest3]:
+    guest.invite()
